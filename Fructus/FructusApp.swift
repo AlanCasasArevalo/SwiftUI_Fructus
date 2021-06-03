1a82591ee08c49591c55ce6589e483bab312642c
Alan Casas <alancasasarevalo@gmail.com>
2021-06-03 15:34:06 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Alan Casas on 03/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
